5f0c8cc8e329e563136fbfff2fc3ec0d4fde126c
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2021-12-09 16:34:59 +0100

    :bug: make the download script more robust

--- copy-data-locally.sh
+++ copy-data-locally.sh
@@ -1,16 +1,31 @@
 runlist=${1:-run.list}
 out=${2:-curl.config.txt}
+checksums=${3:-checksums.txt}
 
-rm $out
+command -v md5 && MD5COMMAND=md5 || MD5COMMAND=md5sum
+
+rm -f $out
 mkdir -p data
 
 for run in $(cat $runlist | tr "," " "); do
   src="https://cernbox.cern.ch/index.php/s/r7VFXonK39smzKP/download?path=${run}/AnalysisResults.root"
   dest="data/run${run}.data.root"
-  echo url = \"$src\" >> $out
-  echo output = \"$dest\" >> $out
+  transfert="no"
+  if test -f $dest; then
+    # file already there. check its checksum
+    ref_checksum=$(grep $dest $checksums | cut -d ' ' -f 1)
+    checksum=$(${MD5COMMAND} $dest | cut -d ' ' -f 1)
+    if [ "$ref_checksum" != "$checksum" ]; then
+       echo "File $dest already there but with wrong checksum : will retransfert it"
+       transfert="yes"
+    fi
+  fi
+  if [ "$transfert" = "yes" ]; then
+    echo url = \"$src\" >> $out
+    echo output = \"$dest\" >> $out
+  fi
 done
 
-time curl -Z -# -K $out
+test -f $out && time curl -Z -# -K $out
 
-rm $out
+rm -f $out
