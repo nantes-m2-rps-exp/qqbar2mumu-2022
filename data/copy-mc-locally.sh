380afb43c111d11f0fdad3c283fe071fefc9bf4d
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2022-11-10 05:21:57 +0100

    first version of mc download

---
+++ data/copy-mc-locally.sh
@@ -0,0 +1,2 @@
+MYDIR="$(dirname $(realpath $0))"
+source $MYDIR/copy.sh mc $1 $2 $3 $4
---
+++ data/copy.sh
@@ -0,0 +1,42 @@
+type=${1:-data}
+runlist=${2:-run.list}
+out=${3:-curl.config.${type}.txt}
+checksums_template=${4:-checksums.${type}.txt.ini}
+dest_dir=${5:-/data}
+
+command -v md5 > /dev/null && MD5COMMAND=md5 || MD5COMMAND=md5sum
+
+rm -f $out
+mkdir -p $dest_dir || exit
+
+rm -f checksums.${type}.txt
+cat ${checksums_template} | sed s+DESTDIR+${dest_dir}+ > checksums.${type}.txt
+
+baselink="https://cernbox.cern.ch/remote.php/dav/public-files/JIjQaAYEQnmRDkX/"
+if [ "$type" = "mc" ]; then
+	baselink="https://cernbox.cern.ch/remote.php/dav/public-files/aCVOXHKOKNOpWDP"
+fi
+
+for run in $(cat $runlist | tr "," " "); do
+  src="${baselink}/${run}/AnalysisResults.root"
+  dest="${dest_dir}/run${run}.${type}.root"
+  transfert="yes"
+  if test -f $dest; then
+    # file already there. check its checksum
+    ref_checksum=$(grep $dest checksums.${type}.txt | cut -d ' ' -f 1)
+    checksum=$(${MD5COMMAND} $dest | cut -d ' ' -f 1)
+    if [ "$ref_checksum" == "$checksum" ]; then
+       echo "file:$dest already downloaded successfully"
+       transfert="no"
+    fi
+  fi
+  if [ "$transfert" = "yes" ]; then
+    echo "file:$dest will need to be downloaded"
+    echo url = \"$src\" >> $out
+    echo output = \"$dest\" >> $out
+  fi
+done
+
+test -f $out && time curl -Z -# -K $out
+
+rm -f $out
